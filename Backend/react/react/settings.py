0a7090b4b5001ba215ccfd8418662eb8ed26747e
ABHAY-st2580 <abhaysinghtomar835@gmail.com>
2026-05-08 20:34:52 +0530

    Postgres added for database deploy...

--- Backend/react/react/settings.py
+++ Backend/react/react/settings.py
@@ -125,7 +125,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://my-portfolio-pied-one-11.vercel.app"
 ]
-
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Internationalization
 
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
